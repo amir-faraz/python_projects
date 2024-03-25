6197e18bce6e8ed3b1bd1d9312f03784b315fa9a
Amir Kazemi Faraz <55465574+amir-faraz@users.noreply.github.com>
2024-03-25 14:19:28 -0400

    Add files via upload

---
+++ brand name generator/main.py
@@ -0,0 +1,9 @@
+#greeting
+print("Welcome to the first python program.")
+#city that they grew up in.
+city_name = input("Which city you were born? \n")
+#name of the user's pet.
+pet_name = input("What is your pet name? \n")
+#Combine the name of their city and pet
+print(f"Your city is {city_name} & Your pet name is {pet_name}")
+
